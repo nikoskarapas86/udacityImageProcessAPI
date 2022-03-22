bd474b8d2731224b944edde636bcd1de7e04a483
nikolaos karapas <nikos.karapas86@gmail.com>
2022-03-22 16:23:44 +0200

    in progress

--- src/routes/api/imageForProcess.ts
+++ src/routes/api/imageForProcess.ts
@@ -3,9 +3,12 @@ import url from 'url';
 import ImageQueryParams from '../../models/imageQueryParams';
 import sharpProcess from '../../utilities/sharpProcess';
 import validator from '../../utilities/validator';
+
+
 const imageForProcess = express.Router();
 
 const validQueryParams = (query: ImageQueryParams, res: express.Response):void => {
+
   sharpProcess.resizeImage(query).then((response) => {
     if (response) {
       const target: string = `/assets/thumb/${query.target}_${query.width}x${query.height}.png`.toString();
@@ -14,12 +17,15 @@ const validQueryParams = (query: ImageQueryParams, res: express.Response):void =
   });
 };
 const invalidQueryParams = (res: express.Response):void => {
-  res.send('invalid width or height');
+  res.send('invalid width or height or this target its not included to our database');
 };
 const processImage = (query: ImageQueryParams, res: express.Response):void => {
-  validator.validateNumber(query.height) && validator.validateNumber(query.width) && validator.validateTarget(query.target)
-    ? validQueryParams(query, res)
+  validator.validateTarget(query).then(response =>{
+   
+    response?  validQueryParams(query, res)
     : invalidQueryParams(res);
+  })
+   
 };
 
 imageForProcess.get('/', (req:express.Request, res: express.Response) => {
--- src/utilities/validator.ts
+++ src/utilities/validator.ts
@@ -1,9 +1,33 @@
+ import { promises as fsPromises } from 'fs';
+import path from "path";
+import ImageQueryParams from '../models/imageQueryParams';
+
+
+const imagesPath = path.resolve(__dirname, '../../assets/images/');
+
+  const getAvailableImageNames=async(): Promise<string[]>=> {
+  try {
+    return (await fsPromises.readdir(imagesPath)).map(
+      (name: string): string => name.split('.')[0]
+    ); 
+  } catch {
+    return [];
+  }
+}
+
 const validateNumber = (num: string): boolean => {
   const widthOrHeight: number = parseInt(num);
   return !isNaN(widthOrHeight) && widthOrHeight > 0;
 };
-const validateTarget = (target: string): boolean => {
-  const images:string[] =['encenadaport','fjord','icelandwaterfall','palmtunnel','santamonica']
-  return images.includes(target)
+
+const validateTarget =async (query: ImageQueryParams): Promise<boolean | string> => {
+  try {
+ const availableImages = await getAvailableImageNames();
+ return availableImages.includes(query.target) && validateNumber(query.height) && validateNumber(query.width);
+  } 
+  catch(error){
+    return "an error is been occured"
+  }
+ 
 };
 export default { validateNumber,validateTarget };
