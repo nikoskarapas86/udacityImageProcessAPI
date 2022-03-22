360b24111f101e0dc3a4addbae7b3fc290ad35ef
nikolaos karapas <nikos.karapas86@gmail.com>
2022-03-22 19:42:29 +0200

    folder created dynamically

--- src/routes/api/imageForProcess.ts
+++ src/routes/api/imageForProcess.ts
@@ -1,12 +1,22 @@
 import express from 'express';
+import { promises as fsPromises } from 'fs';
+import path from 'path';
 import url from 'url';
 import ImageQueryParams from '../../models/imageQueryParams';
 import sharpProcess from '../../utilities/sharpProcess';
 import validator from '../../utilities/validator';
-
 const imageForProcess = express.Router();
-
-const validQueryParams = (query: ImageQueryParams, res: express.Response): void => {
+const createThumb = async (): Promise<boolean> => {
+  const imagesThumbPath = path.resolve('.', './assets/thumb');
+  try {
+    await fsPromises.access(imagesThumbPath);
+    return true;
+  } catch {
+    fsPromises.mkdir(imagesThumbPath);
+  }
+  return false;
+};
+const resizeImage = (query: ImageQueryParams, res: express.Response) => {
   sharpProcess.resizeImage(query).then((response) => {
     if (response) {
       const target: string = `/assets/thumb/${query.target}_${query.width}x${query.height}.png`.toString();
@@ -14,6 +24,11 @@ const validQueryParams = (query: ImageQueryParams, res: express.Response): void 
     }
   });
 };
+const validQueryParams = async (query: ImageQueryParams, res: express.Response): Promise<void> => {
+  const existsFile: boolean = await createThumb();
+  existsFile ? resizeImage(query, res) : null;
+};
+
 const invalidQueryParams = (res: express.Response): void => {
   res.send('invalid width or height or this target its not included to our database');
 };
